9b9955c265f0d3d230c4ef268fcb32fceb593948
bastionwolfe <166756081+bastionwolfe@users.noreply.github.com>
2025-09-30 14:31:54 -0600

    Create PhysicObjects.h
    Started on the phyicobjects for each data struct type used in vector3. SoAoS8 will be added either tonight or tomorrow. I will be hopefully creating a mathutils.h to throw my sqrtapprox in that lives in vector3.h
    
    I currently have two workstations. This on that I am at doesn't support vsc or an equivalent. So these uploads are done on cpp.sh which support one file at a time. When I get to my other workstation that has vsc, I will properly set it up. Testing of Vector3 and PhysicObjects will take place after they have been implemented
    
    also euler is what is being used currently but will implement others like verlet

---
+++ Isometric-Physics-Engine/include/PhysicObjects.h
@@ -0,0 +1,188 @@
+//physobjs
+#include <vector>
+#include "Vector3.h"
+
+class PhysicsObjAoS {
+    public:
+    AoS_Vec3 position;
+    AoS_Vec3 velocity;
+    AoS_Vec3 acceleration;
+    AoS_Vec3 forceAccumulator;
+    
+    float mass;
+    float inverseMass;
+    float friction;
+    float restitution;
+    
+    PhysicsObjAoS(AoS_Vec3 pos, float objMass) {
+        position = pos;
+        velocity = AoS_Vec3(0.00f, 0.00f, 0.00f);
+        acceleration = AoS_Vec3(0.00f, 0.00f, 0.00f);
+        forceAccumulator = AoS_Vec3(0.00f, 0.00f, 0.00f);
+        mass = objMass;
+        inverseMass = (mass != 0.0f) ? 1.0f / mass : 0.0f;
+        friction = 0.50f;
+        restitution = 0.50f;
+    }
+    
+    PhysicsObjAoS(const PhysicsObjAoS& ObjAoS) {
+        position = ObjAoS.position;
+        velocity = ObjAoS.velocity;
+        acceleration = ObjAoS.acceleration;
+        forceAccumulator = ObjAoS.forceAccumulator;
+        mass = ObjAoS.mass;
+        inverseMass = ObjAoS.inverseMass;
+        friction = ObjAoS.friction;
+        restitution = ObjAoS.restitution;
+    }
+    
+    PhysicsObjAos& operator =(const PhysicsObjAoS& ObjAoS) {
+        if (this != &ObjAoS) {
+            position = ObjAoS.position;
+            velocity = ObjAoS.velocity;
+            acceleration = ObjAoS.acceleration;
+            forceAccumulator = ObjAoS.forceAccumulator;
+            mass = ObjAoS.mass;
+            inverseMass = ObjAoS.inverseMass;
+            friction = ObjAoS.friction;
+            restitution = ObjAoS.restitution;
+        }
+        return *this;
+    }
+    
+    void integrateVelocity(float dt) {
+        velocity += acceleration * dt;
+    }
+    
+    void integratePosition(float dt) {
+        position += velocity * dt;
+    }
+    
+    void clearForce() {
+        forceAccumulator = AoS_Vec3(0.00f, 0.00f, 0.00f);
+    }
+    
+    void applyForce(float xForce, float yForce, float zForce) {
+        forceAccumulator.x = xForce;
+        forceAccumulator.y = yForce;
+        forceAccumulator.z = zForce;
+    }
+    
+    void applyFriction() {
+    }
+    
+    void setRestitution(float r) {
+        if (r < 0 || r > 1) return;
+        restitution = r;
+    }
+    
+    void updateAcceleration() {
+        if (mass == 0) return;
+        accelerations[i] = forceAccumulator[i] * inverseMasses[i];
+        clearForce[i];
+    }
+    
+};
+
+class PhysicsObjSoA {
+public:
+// include mass, inverse mass, friction, resitution, force accumilator
+    std::vector<SoA_Vec3> positions;
+    std::vector<SoA_Vec3> velocities;
+    std::vector<SoA_Vec3> accelerations;
+    std::vector<SoA_Vec3> forceAccumulator;
+    
+    std::vector<float> masses;
+    std::vector<float> inverseMasses;
+    std::vector<float> friction;
+    std::vector<float> restitution;
+
+    PhysicsObjSoA(int numObjects) {
+        positions.resize(numObjects);
+        velocities.resize(numObjects);
+        accelerations.resize(numObjects);
+        forceAccumulator.resize(numObjects);
+        masses.resize(numObjects, 1.0f);
+        inverseMasses.resize(numObjects, 1.0f);
+        friction.resize(numObjects, 0.5f);
+        restitution.resize(numObjects, 0.5f);
+    }
+    
+    PhysicsObjSoA(const PhysicsObjSoA& ObjSoA) 
+        : positions(ObjSoA.positions), velocities(ObjSoA.velocities), accelerations(ObjSoA.accelerations), forceAccumulator(ObjSoA.forceAccumulator),
+         masses(ObjSoA.masses), inverseMasses(ObjSoA.inverseMasses), friction(ObjSoA.friction), restitution(ObjSoA.restitution) {}
+
+    PhysicsObjSoA& operator=(const PhysicsObjSoA& ObjSoA) {
+        if (this != &ObjSoA) {
+            positions = ObjSoA.positions;
+            velocities = ObjSoA.velocities;
+            accelerations = ObjSoA.accelerations;
+            forceAccumulator = ObjSoA.forceAccumulator;
+            masses = ObjSoA.masses;
+            inverseMasses = ObjSoA.inverseMasses;
+            friction = ObjSoA.friction;
+            restitution = ObjSoA.restitution;
+        }
+        return *this;
+    }
+
+
+    void integrateVelocities(float dt) {
+        for (size_t i = 0; i < velocities.size(); ++i) {
+            velocities[i] += accelerations[i] * dt;
+        }
+    }
+
+    void integratePositions(float dt) {
+        for (size_t i = 0; i < positions.size(); ++i) {
+            positions[i] += velocities[i] * dt;
+        }
+    }
+    
+    int getSize() const {
+        return static_cast<int>(positions.size());
+    }
+    
+    void inverseMassCalc() {
+        for (int i = 0; i < masses.size(); ++i) {
+            if (masses[i] == 0) {
+                inverseMasses[i] = 0;
+            }
+            else {
+                inverseMasses[i] = 1 / masses[i];
+            }
+        }
+    }
+    
+    void clearForce(int i) {
+        forceAccumulator[i].x = 0;
+        forceAccumulator[i].y = 0;
+        forceAccumulator[i].z = 0;
+    }
+    
+    void applyForce(int i, float xForce, float yForce, float zForce) {
+        forceAccumulator[i].x += xForce;
+        forceAccumulator[i].y += yForce;
+        forceAccumulator[i].z += zForce;
+    }
+    
+    void applyFriction() {
+        
+    }
+    
+    void setRestitutions(float r, int i) {
+        if (r < 0 || r > 1) return;
+        if (i < 0 || i >= restitutions.size()) return;
+        restitutions[i] = r;
+    }
+    
+    void updateAccelerations() {
+        if (positions.size() == forceAccumulator.size() && forceAccumulator.size() == masses.size()) {
+            for (int i = 0; i < positions.size(); ++i) {
+                if (masses[i] == 0.00f) continue;
+                accelerations[i] = forceAccumulator[i] * inverseMasses[i];
+                clearForce[i];
+            }
+        }
+    }
+};
