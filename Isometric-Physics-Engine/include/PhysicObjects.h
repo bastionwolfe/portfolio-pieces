143acf9de4be12fc3bcda7e4ea7aeea5223f814c
bastionwolfe <166756081+bastionwolfe@users.noreply.github.com>
2025-10-01 10:43:22 -0600

    Update PhysicObjects.h
    added SoAoS8 into this. It isn't SIMD compatible for now but it is friendly. once I go back to vector3 to fine tune it and add more SoAoS8 functions, I will make it work with SIMD better

--- Isometric-Physics-Engine/include/PhysicObjects.h
+++ Isometric-Physics-Engine/include/PhysicObjects.h
@@ -85,8 +85,8 @@ class PhysicsObjAoS {
 };
 
 class PhysicsObjSoA {
-public:
-// include mass, inverse mass, friction, resitution, force accumilator
+    public:
+    // include mass, inverse mass, friction, resitution, force accumilator
     std::vector<SoA_Vec3> positions;
     std::vector<SoA_Vec3> velocities;
     std::vector<SoA_Vec3> accelerations;
@@ -185,4 +185,135 @@ public:
             }
         }
     }
+};
+
+class PhysicsObjSoAoS8 {
+    public:
+    SoAoS8 positions;
+    SoAoS8 velocities;
+    SoAoS8 accelerations;
+    SoAoS8 forceAccumulators;
+    
+    std::vector<float> masses;
+    std::vector<float> inverseMasses;
+    std::vector<float> friction;
+    std::vector<float> restitution;
+    
+    PhysicsObjSoAoS8(int numObjects)
+    : positions(numObjects),
+      velocities(numObjects),
+      accelerations(numObjects),
+      forceAccumulators(numObjects),
+      masses(numObjects, 1.0f),
+      inverseMasses(numObjects, 1.0f),
+      friction(numObjects, 0.5f),
+      restitution(numObjects, 0.5f) {}
+    
+    PhysicsObjSoAoS8(const PhysicsObjSoAoS8& ObjSoAoS8) {
+        positions = ObjSoAoS8.positions;
+        velocities = ObjSoAoS8.velocities;
+        accelerations = ObjSoAoS8.accelerations;
+        forceAccumulators = ObjSoAoS8.forceAccumulators;
+        masses = ObjSoAoS8.masses;
+        inverseMasses = ObjSoAoS8.inverseMasses;
+        friction = ObjSoAoS8.friction;
+        restitution = ObjSoAoS8.restitution;
+    }
+    
+    PhysicsObjSoAoS8& operator =(const PhysicsObjSoAoS8& ObjSoAoS8) {
+        if (this != ObjSoAoS8) {
+            positions = ObjSoAoS8.positions;
+            velocities = ObjSoAoS8.velocities;
+            accelerations = ObjSoAoS8.accelerations;
+            forceAccumulators = ObjSoAoS8.forceAccumulators;
+            masses = ObjSoAoS8.masses;
+            inverseMasses = ObjSoAoS8.inverseMasses;
+            friction = ObjSoAoS8.friction;
+            restitution = ObjSoAoS8.restitution;
+        }
+        return *this;
+    }
+    
+    void integrateVelocities(float dt) {
+        int objectCount = velocities.getSize();
+
+        for (int i = 0; i < objectCount; ++i) {
+            float vx = velocities.getX(i);
+            float vy = velocities.getY(i);
+            float vz = velocities.getZ(i);
+            float ax = accelerations.getX(i);
+            float ay = accelerations.getY(i);
+            float az = accelerations.getZ(i);
+            velocities.setX(i, vx + ax * dt);
+            velocities.setY(i, vy + ay * dt);
+            velocities.setZ(i, vz + az * dt);
+        }
+    }  
+    
+    void integratePositions(float dt) {
+        int objectCount = positions.getSize();
+        
+        for (int i = 0; i < objectCount; ++i) {
+            float px = positions.getX(i);
+            float py = positions.getY(i);
+            float pz = positions.getZ(i);
+            float vx = velocities.getX(i);
+            float vy = velocities.getY(i);
+            float vz = velocities.getZ(i);
+            positions.setX(i, px + vx * dt);
+            positions.setY(i, py + vy * dt);
+            positions.setZ(i, pz + vz * dt);
+        }
+       
+        
+    }
+    
+    void inverseMassCalc() {
+        for (int i = 0; i < masses.size(); ++i) {
+            if (masses[i] == 0) {
+                inverseMasses[i] = 0;
+            }
+            else {
+                inverseMasses[i] = 1 / masses[i];
+            }
+        }
+    }
+    
+    void clearForce() {
+        int forceSize = forceAccumulators.getSize();
+        for (int i = 0; i < forceSize; ++i) {
+            forceAccumulator.setX(i, 0);
+            forceAccumulator.setY(i, 0);
+            forceAccumulator.setZ(i, 0);
+        }
+    }
+    
+    void applyForce(int i, float xForce, float yForce, float zForce) {
+        forceAccumulator.setX(i, xForce);
+        forceAccumulator.setY(i, yForce);
+        forceAccumulator.setZ(i, zForce);
+    }
+    
+    void applyFriction() {
+    }
+    
+    void setRestitutions(float r, int i) {
+        if (r < 0 || r > 1) return;
+        if (i < 0 || i >= restitution.size()) return;
+        restitution[i] = r;
+    }
+    
+    void updateAccelerations() {
+        int posSize = positions.getSize();
+        if (posSize == forceAccumulator.getSize() && posSize == masses.size()) {
+            for (int i = 0; i < posSize; ++i) {
+                if (masses[i] == 0.00f) continue;
+                accelerations.setX(i, forceAccumulator.getX(i) * inverseMasses.getX(i));
+                accelerations.setY(i, forceAccumulator.getY(i) * inverseMasses.getY(i));
+                accelerations.setZ(i, forceAccumulator.getZ(i) * inverseMasses.getZ(i));
+            }
+            clearForce();
+        }
+    }
+    
 };
