a781b7de8cb2510f074ad746f001a049edb5912b
bastionwolfe <166756081+bastionwolfe@users.noreply.github.com>
2025-10-02 07:48:54 -0600

    Update Vector3.h
    removed the sqrtapprox and moved it into its own header named physMathUtils.h

--- Isometric-Physics-Engine/include/Vector3.h
+++ Isometric-Physics-Engine/include/Vector3.h
@@ -13,21 +13,7 @@
 #pragma once
 #include <vector>
 #include <iostream>
-
-//square root finder
-float sqrt_approx(float number) {
-    if (number <= 0) return 0;
-
-    float x = number;
-    float y = 1.0f;
-    const float epsilon = 0.00001f;  // precision
-
-    while (x - y > epsilon) {
-        x = (x + y) * 0.5f;
-        y = number / x;
-    }
-    return x;
-}
+#include "physMathUtils.h"
 
 //Array of structures
 class AoS_Vec3 {
